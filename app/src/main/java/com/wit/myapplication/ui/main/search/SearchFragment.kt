ae981dd332e8a50d44c2433ca4ca420a55c6c2e8
Balavadze <98849484+balavadze@users.noreply.github.com>
2023-01-22 17:15:08 +0100

    search with NO RESULT function works

--- app/src/main/java/com/wit/myapplication/ui/main/search/SearchFragment.kt
+++ app/src/main/java/com/wit/myapplication/ui/main/search/SearchFragment.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -17,7 +18,6 @@ import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
     private val adapter = ProductAdapter(::product)
-
     private lateinit var viewModel: SearchViewModel
     private lateinit var binding: FragmentSearchBinding
 
@@ -27,7 +27,6 @@ class SearchFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewModel = ViewModelProvider(this)[SearchViewModel::class.java]
-
     }
 
     override fun onCreateView(
@@ -42,21 +41,43 @@ class SearchFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.searchRecycler.adapter = adapter
 
-
-
         lifecycleScope.launch {
             viewModel.searchedProducts.collectLatest { product ->
                 adapter.data = product
+
                 adapter.notifyDataSetChanged()
+                if (product.isEmpty()) {
+
+                } else {
+                    binding.searchRecycler.visibility = View.VISIBLE
+                    binding.noResults.visibility = View.GONE
+                }
             }
         }
+
         binding.searchEditText.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
             }
 
             override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.search(s?.toString() ?: "")
+                if (!s.isNullOrEmpty()) {
+                    viewModel.search(s.toString())
+                    var found = false
+                    for (product in adapter.data) {
+                        if (product.productDescription.contains(s.toString(), ignoreCase = true)) {
+                            found = true
+                            break
+                        }
+                    }
+                    if (!found) {
+                        binding.searchRecycler.visibility = View.GONE
+                        binding.noResults.visibility = View.VISIBLE
+                    } else {
+                        binding.searchRecycler.visibility = View.VISIBLE
+                        binding.noResults.visibility = View.GONE
+                    }
+                }
             }
 
             override fun afterTextChanged(p0: Editable?) {
