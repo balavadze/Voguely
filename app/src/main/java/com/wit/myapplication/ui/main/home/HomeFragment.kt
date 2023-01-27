b573ffbcac43a89eaf045ccd6648592ced6b830d
Balavadze <98849484+balavadze@users.noreply.github.com>
2023-01-27 10:01:51 +0100

    toolbar on half way

--- app/src/main/java/com/wit/myapplication/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/wit/myapplication/ui/main/home/HomeFragment.kt
@@ -34,8 +34,6 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.recyclerView.adapter = adapter
 
-
-
         lifecycleScope.launch {
             viewModel.product.collectLatest { product ->
                 adapter.data = product
