c7b951c93a45291c6706487efd3f274e965bf41b
Balavadze <98849484+balavadze@users.noreply.github.com>
2023-02-01 11:22:25 +0100

    layout works

--- app/src/main/java/com/wit/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/wit/myapplication/ui/main/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.wit.myapplication.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
@@ -10,38 +9,24 @@ import androidx.navigation.ui.setupWithNavController
 import com.wit.myapplication.R
 import com.wit.myapplication.databinding.FragmentMainBinding
 
+@Suppress("DEPRECATION")
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.toolbar, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.tv_sign_out -> {
-                Log.e("toolbar", "sign out clicked" )
-            }
-        }
-        return true
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
+        setHasOptionsMenu(true)
         binding = FragmentMainBinding.inflate(inflater, container, false)
-
-
         return binding.root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,8 +35,9 @@ class MainFragment : Fragment() {
             childFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
         val navController = navHostFragment.findNavController()
         binding.bottomNavigationView.setupWithNavController(navController)
-
-
     }
+}
+
+private fun signOutFromApp() {
 
 }
